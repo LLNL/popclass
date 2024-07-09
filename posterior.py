d0094734651559eeb296a99cd6359d631a955747
Peter Mcgill <mcgill5@llnl.gov>
2024-07-09 11:32:21 -0700

    Update file posterior.py

--- posterior.py
+++ posterior.py
@@ -3,16 +3,62 @@
 
 
 
-Class Posterior:
+class Posterior:
+
+"""
+Our set internal posterior object.
+
 
 """
 
-"""
+def __init__(posterior_samples, parameter_labels):
+    """
+    Target usage.
 
-def __init__(posterior_samples, parameter_labels)
+    post = Posterior(posterior_samples=np.array(ndim,nsamples), parameter_labels=['tE','piE', 'uO'])
+
+    """
+    pass
+
+
+def marignal(parameter_list):
+    """
+    We probably only want to classify using a slice of the full posterior in a couple of paramters
+
+    target usage.
+
+    marginal = Posterior.marginal(['tE', 'PiE']) should return the tE, Pi marginal distribution. 
+
+    we also want support to take the log transform of the posterior (common use case, this might be hard to do 
+    genreally.)
+
+    """
+    pass
 
 
 
+def convert_arviz(ariz_posterior_object):
+    """
+    function should covert arviz posterior object to our definition of Posterior.
+    """
+    pass
+
+def convert_emcee(emcee_posterior_object):
+    """
+    function should covert emcee posterior object to our definition of Posterior.
+    """
+    pass
 
 
-def marignal(parameter_list)
+def convert_from_dynesty(dynesty_posterior_object):
+    """
+    function should covert dynesty posterior object to our definition of Posterior.
+    """
+    pass
+
+
+def convert_from_pymulitnest(pymultinest_posterior_object):
+    """
+    function should covert pymulitnest posterior object to our definition of Posterior.
+    """
+    pass
