2dc82d7083ff6788db0cfb1264ef4b5a22c087f0
Peter Mcgill <mcgill5@llnl.gov>
2024-07-09 14:34:59 -0700

    Update 4 files
    - /popclass/density.py
    - /tests/test_posterior.py
    - /tests/test_model.py
    - /tests/test_classify.py

---
+++ popclass/density.py
@@ -0,0 +1,13 @@
+class DensityEstimator:
+    """
+    Internal interface for for Kernal Density estimator.
+    """
+
+    def __init__(dataset):
+        pass
+    
+
+    def logpdf(dataset, point):
+        pass
+
+
