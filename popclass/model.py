e3ee335e4144b612a41cff0178d8d0372a227107
gsallabe <sallaberry1@NH-Toucans.the-lab.llnl.gov>
2024-08-06 15:04:19 -0700

    Add population model data in popclass/data

--- popclass/model.py
+++ popclass/model.py
@@ -1,3 +1,11 @@
 """
 Reading, saving, and  handling population models 
 """
+import asdf
+import numpy as np
+
+def load_population_model(model):
+
+    pth = f'data/popsycle_singles_{model}.asdf'
+
+    f = 
