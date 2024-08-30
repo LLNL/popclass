3b8983ea008d303daa51185c5c9151805a631230
Zofia Kaczmarek <zofiak@zah-153.zah.uni-heidelberg.de>
2024-08-30 17:20:02 +0200

    Added tests for correct visualisation axes and bounds

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,13 +1,29 @@
 """
 Test to check visualization.py works
 """
-
+import sys
+sys.path.append("..")
 from popclass.model import PopulationModel
 from popclass.visualization import plot_population_model
 import matplotlib.pyplot as plt
+import numpy as np
 
 def test_plotting_figure():
     popmodel = PopulationModel.from_library('popsycle_singles_sukhboldn20')
-    fig, ax = plot_population_model(PopulationModel=popmodel, parameters=['log10tE', 'log10piE'], plot_samples=True, bounds=None, legend=True)
-    assert(plt.gcf().number == 1)
-
+    parameters = parameters=['log10tE', 'log10piE']
+    fig, ax = plot_population_model(PopulationModel=popmodel, parameters=parameters)
+    assert(ax.figure == fig)
+    
+def test_parameters():
+    popmodel = PopulationModel.from_library('popsycle_singles_sukhboldn20')
+    parameters = parameters=['log10tE', 'log10piE']
+    fig, ax = plot_population_model(PopulationModel=popmodel, parameters=parameters)
+    assert([ax.get_xlabel(), ax.get_ylabel()] == parameters)
+    
+def test_bounds():
+    popmodel = PopulationModel.from_library('popsycle_singles_sukhboldn20')
+    parameters = parameters=['log10tE', 'log10piE']
+    bounds = np.array([[0, 3], [-2, 0]])
+    fig, ax = plot_population_model(PopulationModel=popmodel, parameters=parameters, bounds = bounds)
+    figure_bounds = np.array([ax.get_xbound(), ax.get_ybound()])
+    np.testing.assert_almost_equal(figure_bounds, bounds)
