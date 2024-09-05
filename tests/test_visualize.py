0e8b0945c5481dc4dbca8c706c5ba39c75443a94
Zofia Kaczmarek <zofiak@zah-153.zah.uni-heidelberg.de>
2024-09-05 15:28:35 +0200

    Tests added to cover multiple plotting input options in visualization.py

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -5,6 +5,7 @@ import pytest
 import numpy as np
 from popclass.model import PopulationModel
 from popclass.visualization import plot_population_model
+from unittest import TestCase
 
 def test_plot_population_model():
     """
@@ -34,6 +35,16 @@ def test_parameters():
     fig, ax = plot_population_model(PopulationModel=popmodel, parameters=parameters)
     assert([ax.get_xlabel(), ax.get_ylabel()] == parameters)
     
+def test_1D_parameter():
+    """
+    Check 1D figure is plotted and labels match a density histogram of parameter
+    """
+    popmodel = PopulationModel.from_library('popsycle_singles_sukhboldn20')
+    parameters = ['log10tE']
+    fig, ax = plot_population_model(PopulationModel=popmodel, parameters=parameters)
+    assert(ax.get_xlabel() == parameters[0])
+    assert(ax.get_ylabel() == 'density')
+    
 def test_bounds():
     """
     Check figure bounds are adjusted as specified
@@ -44,3 +55,14 @@ def test_bounds():
     fig, ax = plot_population_model(PopulationModel=popmodel, parameters=parameters, bounds = bounds)
     figure_bounds = np.array([ax.get_xbound(), ax.get_ybound()])
     np.testing.assert_almost_equal(figure_bounds, bounds)
+
+class TestDimensionError(TestCase):
+    def test_dimensions(self):
+        """
+        Check that ValueError is raised if too many parameters given to visualize
+        """
+        popmodel = PopulationModel.from_library('popsycle_singles_sukhboldn20')
+        parameters = ['log10tE', 'log10piE', 'f_blend_I']
+        with self.assertRaises(ValueError):
+            fig, ax = plot_population_model(PopulationModel=popmodel, parameters=parameters)
+    
