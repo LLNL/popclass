9c254356f230560f7c31a90f794e44595555b109
Scott Perkins <perkins35@llnl.gov>
2024-09-08 11:18:49 -0700

    Switching to a class structure for additive uq for additional flexibility down the road

--- popclass/uq.py
+++ popclass/uq.py
@@ -4,79 +4,94 @@ The classification framework is susceptible to systematic error through a variet
 import numpy as np
 from scipy.stats import gaussian_kde
 
-def apply_none_class_uq(unnormalized_prob,inference_data, population_model, parameters, bounds, kde=gaussian_kde):
-    """
-    Applies ``None'' class uncertainty quantification to the classification results. 
-    The method transforms the initial classification result, using the base population model.
-    Works by defining an additional class to the set of classes, defined as the areas of parameter 
-    space poorly supported by the base population model.
+class additiveUQ():
+    def __init__(self):
+        return
 
-    Args:
-        unnormalized_prob (dictionary):
-            Dictionary containing initial classification results, performed with the base population model.
-        inference_data (popclass.InferenceData):
-            popclass InferenceData object
-        population_model (popclass.PopulationModel):
-            popclass PopulationModel object
-        parameters (list):
-            Parameters to use for classification.
-        bounds (dictionary):
-            Dictionary containing the lower and upper bounds of the parameter space, with keys
-            matching the supplied ``parameter'' list. Format: {key : [lower_bound, upper_bound]}
+    def apply_uq(self,unnormalized_prob,inference_data, population_model, parameters):
+        return unnormalized_probs 
 
-    Returns:
-        Dictionary of classes in ``PopulationModel.classes()`` and associated 
-        probability, unnormalized, with the appended ``None'' class and associated probability.
-    
-    """
-    # Tunable Hyperparameters: kde method, kde kernel (maybe), kde bandwidth, prior weight
-    grid_size = int(1e3)
-    kde = gaussian_kde
-    kde_kwargs = {}
+class NoneClassUQ(additiveUQ):
+    def __init__(self,bounds, grid_size=int(1e3), kde=gaussian_kde, kde_kwargs={"bandwidth":0.4}):
+        self.grid_size = grid_size
+        self.kde = kde
+        self.kde_kwargs = kde_kwargs
+        return
+    def apply_uq(self, unnormalized_prob,inference_data, population_model, parameters):
+        """
+        Applies ``None'' class uncertainty quantification to the classification results. 
+        The method transforms the initial classification result, using the base population model.
+        Works by defining an additional class to the set of classes, defined as the areas of parameter 
+        space poorly supported by the base population model.
+
+        Args:
+            unnormalized_prob (dictionary):
+                Dictionary containing initial classification results, performed with the base population model.
+            inference_data (popclass.InferenceData):
+                popclass InferenceData object
+            population_model (popclass.PopulationModel):
+                popclass PopulationModel object
+            parameters (list):
+                Parameters to use for classification.
+            bounds (dictionary):
+                Dictionary containing the lower and upper bounds of the parameter space, with keys
+                matching the supplied ``parameter'' list. Format: {key : [lower_bound, upper_bound]}
+
+        Returns:
+            Dictionary of classes in ``PopulationModel.classes()`` and associated 
+            probability, unnormalized, with the appended ``None'' class and associated probability.
+        
+        """
+        # Tunable Hyperparameters: kde method, kde kernel (maybe), kde bandwidth, prior weight
+        grid_size = int(1e3)
+        kde = gaussian_kde
+        kde_kwargs = {}
 
 
-    # Calculate 1000x1000 grid in parameter space
-    # should be grid_size**d elements
-    grid, grid_mesh, grid_corners = calculate_square_grid_coordinates(gridsize, bounds)
+        # Calculate 1000x1000 grid in parameter space
+        # should be grid_size**d elements
+        grid, grid_mesh, grid_corners = calculate_square_grid_coordinates(gridsize, bounds)
 
-    # Calculate bin centers
-    # should be (grid_size-1)**d elements
-    grid_centers, grid_mesh_centers, grid_centers = calculate_square_grid_centers(grid)
-    #grid_centers = {p : (grid[p][1:] + grid[p][:-1])/2 for p in grid.keys()}
-    #grid_mesh_centers = np.array(np.meshgrid(*list(grid_centers.values())))
-    #grid_centers = np.array([ a.ravel() for a in grid_mesh_centers]).transpose()
+        # Calculate bin centers
+        # should be (grid_size-1)**d elements
+        grid_centers, grid_mesh_centers, grid_centers = calculate_square_grid_centers(grid)
+        #grid_centers = {p : (grid[p][1:] + grid[p][:-1])/2 for p in grid.keys()}
+        #grid_mesh_centers = np.array(np.meshgrid(*list(grid_centers.values())))
+        #grid_centers = np.array([ a.ravel() for a in grid_mesh_centers]).transpose()
 
-    # Calculate grid volumes - Assume fixed grid
-    # should be (grid_size-1)**d elements
-    grid_volumnes = np.product(np.array([grid[p][1]-grid[p][0] for p in grid.keys()])) * np.ones(grid_centers.shape)
+        # Calculate grid volumes - Assume fixed grid
+        # should be (grid_size-1)**d elements
+        grid_volumnes = np.product(np.array([grid[p][1]-grid[p][0] for p in grid.keys()])) * np.ones(grid_centers.shape)
 
-    # Train kde with tophat kernel and bandwidth of .4 on galactic model data
-    pop_model_samples = np.vstack([ population_model.get_samples(class_name, parameters) for class_name in population_model.classes])
-    pop_model_kde = kde(pop_model_samples,**kde_kwargs)
-    pop_model_eval_centers = pop_model_kde(grid_centers)
-    max_pop_model_eval_centers = np.amax(pop_model_eval_centers)
+        # Train kde with tophat kernel and bandwidth of .4 on galactic model data
+        pop_model_samples = np.vstack([ population_model.get_samples(class_name, parameters) for class_name in population_model.classes])
+        pop_model_kde = kde(pop_model_samples,**kde_kwargs)
+        pop_model_eval_centers = pop_model_kde(grid_centers)
+        max_pop_model_eval_centers = np.amax(pop_model_eval_centers)
 
-    # Assign p(phi|NONE) to bin centers
-    
-    none_class_pdf_centers_unnormed = (1. - pop_model_eval_centers/max_pop_model_eval_centers)
-    none_class_pdf_normalization = np.sum(none_class_pdf_centers_unnormed * grid_volumes)
-    none_class_pdf_centers = none_class_pdf_centers_unnormed / none_class_pdf_normalization
+        # Assign p(phi|NONE) to bin centers
+        
+        none_class_pdf_centers_unnormed = (1. - pop_model_eval_centers/max_pop_model_eval_centers)
+        none_class_pdf_normalization = np.sum(none_class_pdf_centers_unnormed * grid_volumes)
+        none_class_pdf_centers = none_class_pdf_centers_unnormed / none_class_pdf_normalization
 
-    # Calculate A such that it's normalized
+        # Calculate A such that it's normalized
 
-    # Assign prior probability of 0.01
+        # Assign prior probability of 0.01
 
-    # Undo prior weighting on unnormalized probability
+        # Undo prior weighting on unnormalized probability
 
-    # Calculate new prior
+        # Calculate new prior
 
-    # Calculate likelihood of event in None class
+        # Calculate likelihood of event in None class
 
-    # Append to dictionary
+        # Append to dictionary
 
-    # reweight by new priors
-    
-    return unnormalized_prob
+        # reweight by new priors
+        
+        return unnormalized_prob
+            
+
 
 def calculate_square_grid_coordinates(grid_size, bounds):
     """
