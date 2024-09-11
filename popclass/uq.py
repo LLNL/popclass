f9bf2e23b897b0b23597efc92051c831db8ae212
Scott Perkins <perkins35@llnl.gov>
2024-09-11 12:14:29 -0700

    Adding to uq None class and adding testing for uq.py

--- popclass/uq.py
+++ popclass/uq.py
@@ -9,19 +9,24 @@ class additiveUQ():
         return
 
     def apply_uq(self,unnormalized_prob,inference_data, population_model, parameters):
-        return unnormalized_probs 
+        return unnormalized_prob 
 
 class NoneClassUQ(additiveUQ):
-    def __init__(self,bounds, grid_size=int(1e3), kde=gaussian_kde, kde_kwargs={"bandwidth":0.4}, population_model =None):
+    #def __init__(self,bounds, grid_size=int(1e3), kde=gaussian_kde, kde_kwargs={"bandwidth":0.4}, population_model =None, parameters=None,none_class_weight = .01):
+    def __init__(self,bounds, grid_size=int(1e3), kde=gaussian_kde, kde_kwargs={}, population_model =None, parameters=None,none_class_weight = .01):
         self.bounds = bounds
         self.grid_size = grid_size
         self.kde = kde
+        self.none_class_weight = none_class_weight
         self.kde_kwargs = kde_kwargs
         self._build_grids()
-        if population_model :
+        if population_model  and parameters :
+            self.parameters = parameters
             self.base_model_kde = self._train_base_model_kde(population_model)
         else:
             self.base_model_kde = None
+            self.parameters = None
+        
         return
 
     def _build_grids(self):
@@ -46,20 +51,21 @@ class NoneClassUQ(additiveUQ):
 
         # Calculate 1000x1000 grid in parameter space
         # should be grid_size**d elements
-        self.grid, self.grid_mesh, self.grid_corners = calculate_square_grid_coordinates(self.gridsize, self.bounds)
+        self.grid, self.grid_mesh, self.grid_corners = calculate_square_grid_coordinates(self.grid_size, self.bounds)
 
         # Calculate bin centers
         # should be (grid_size-1)**d elements
-        self.grid_centers, self.grid_mesh_centers, self.grid_centers = calculate_square_grid_centers(self.grid)
+        self.grid_centers, self.grid_mesh_centers, self.grid_centers_raveled = calculate_square_grid_centers(self.grid)
 
         # Calculate grid volumes - Assume fixed grid
         # should be (grid_size-1)**d elements
-        self.grid_volumnes = np.product(np.array([self.grid[p][1]-self.grid[p][0] for p in self.grid.keys()])) * np.ones(self.grid_centers.shape)
+        self.grid_volumnes = np.prod(np.array([self.grid[p][1]-self.grid[p][0] for p in self.grid.keys()])) * np.ones(self.grid_centers_raveled.shape)
         return
 
     def _train_base_model_kde(self,population_model):
-        pop_model_samples = np.vstack([ population_model.get_samples(class_name, parameters) for class_name in population_model.classes])
-        base_model_kde = self.kde(pop_model_samples,**kde_kwargs)
+        pop_model_samples = np.vstack([ population_model.samples(class_name, self.parameters) for class_name in population_model.classes])
+        base_model_kde = self.kde(pop_model_samples.T,**self.kde_kwargs)
+    
         return base_model_kde
 
         
@@ -91,18 +97,20 @@ class NoneClassUQ(additiveUQ):
         # Train kde with tophat kernel and bandwidth of .4 on galactic model data
         if self.base_model_kde is None:
             self._train_base_model_kde(population_model) 
-        pop_model_eval_centers = self.base_model_kde(self.grid_centers)
+        pop_model_eval_centers = self.base_model_kde(self.grid_centers_raveled)
         max_pop_model_eval_centers = np.amax(pop_model_eval_centers)
 
         # Assign p(phi|NONE) to bin centers
         
         none_class_pdf_centers_unnormed = (1. - pop_model_eval_centers/max_pop_model_eval_centers)
-        none_class_pdf_normalization = np.sum(none_class_pdf_centers_unnormed * grid_volumes)
+        none_class_pdf_normalization = np.sum(none_class_pdf_centers_unnormed * self.grid_volumes)
         none_class_pdf_centers = none_class_pdf_centers_unnormed / none_class_pdf_normalization
 
-        # Calculate A such that it's normalized
-
         # Assign prior probability of 0.01
+        priors= population_model.class_weights
+        prior_norm = np.sum(priors.values)/(1.-self.none_class_weight)
+        priors = {priors[key] / prior_norm for key in priors.keys()}
+        priors["None"] = self.none_class_weight
 
         # Undo prior weighting on unnormalized probability
 
@@ -134,10 +142,10 @@ def calculate_square_grid_coordinates(grid_size, bounds):
             Dictionary containing the grid edges in each dimension. Format: {parameter_key : np.array(size=grid_size)}
 
         grid_mesh (numpy.array):
-            Numpy containing the meshed grid, shape [dimensions, grid_size, grid_size]
+            Numpy containing the meshed grid, shape [dimensions, grid_size, ...] with a total of dimensions + 1 along the first axis
         
         grid_corners (numpy.array): 
-            Numpy array containing the raveled grid corner coordinates, shape [grid_size*grid_size, dimensions]
+            Numpy array containing the raveled grid corner coordinates, shape [grid_size**dimensions, dimensions]
     """
     grid = {p: np.linspace(bounds[p][0],bounds[p][1], grid_size) for p in bounds.keys()}
     grid_mesh = np.array(np.meshgrid(*list(grid.values())))
@@ -153,16 +161,16 @@ def calculate_square_grid_centers(grid):
             dictionary containing the bin edges for each dimension, keyed by the parameter name
         
     Returns:
-        grid (dictionary):
-            Dictionary containing the grid edges in each dimension. Format: {parameter_key : np.array(size=grid_size)}
+        grid_centers (dictionary):
+            Dictionary containing the grid edges in each dimension. Format: {parameter_key : np.array(size=grid_size-1)}
 
-        grid_mesh (numpy.array):
-            Numpy containing the meshed grid, shape [dimensions, grid_size, grid_size]
+        grid_mesh_centers (numpy.array):
+            Numpy containing the meshed grid, shape [dimensions, grid_size-1, ...] with a total of dimensions + 1 along the first axis
         
-        grid_corners (numpy.array): 
-            Numpy array containing the raveled grid corner coordinates, shape [grid_size*grid_size, dimensions]
+        grid_centers_raveled (numpy.array): 
+            Numpy array containing the raveled grid corner coordinates, shape [(grid_size-1)***dimensions, dimensions]
     """
     grid_centers = {p : (grid[p][1:] + grid[p][:-1])/2 for p in grid.keys()}
     grid_mesh_centers = np.array(np.meshgrid(*list(grid_centers.values())))
-    grid_centers = np.array([ a.ravel() for a in grid_mesh_centers]).transpose()
-    return grid_centers, grid_mesh_centers, grid_centers
+    grid_centers_raveled = np.array([ a.ravel() for a in grid_mesh_centers]).transpose()
+    return grid_centers, grid_mesh_centers, grid_centers_raveled
