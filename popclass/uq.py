8398e45fadba03b0d4b723951dcfb72c5fd01817
Scott Perkins <perkins35@llnl.gov>
2024-09-11 07:18:59 -0700

    Continuing work on uq class structure

--- popclass/uq.py
+++ popclass/uq.py
@@ -12,11 +12,57 @@ class additiveUQ():
         return unnormalized_probs 
 
 class NoneClassUQ(additiveUQ):
-    def __init__(self,bounds, grid_size=int(1e3), kde=gaussian_kde, kde_kwargs={"bandwidth":0.4}):
+    def __init__(self,bounds, grid_size=int(1e3), kde=gaussian_kde, kde_kwargs={"bandwidth":0.4}, population_model =None):
+        self.bounds = bounds
         self.grid_size = grid_size
         self.kde = kde
         self.kde_kwargs = kde_kwargs
+        self._build_grids()
+        if population_model :
+            self.base_model_kde = self._train_base_model_kde(population_model)
+        else:
+            self.base_model_kde = None
         return
+
+    def _build_grids(self):
+        """ 
+            Calculate the square grid (corners and centers) bounded by bounds.
+            Populates the quantities:
+            
+            1. self.grid  (Dictionary containing the grid edges in each dimension. Format: {parameter_key : np.array(size=grid_size)})
+
+            2. self.grid_mesh (Numpy containing the meshed grid, shape [dimensions, grid_size, grid_size])
+            3. self.grid_corners (Numpy array containing the raveled grid corner coordinates, shape [grid_size**dimensions, dimensions])
+            4. self.grid _centers (Dictionary containing the grid centers in each dimension. Format: {parameter_key : np.array(size=grid_size-1)})
+            5. self.grid_mesh_centers (umpy containing the meshed grid centers, shape [dimensions, grid_size-1, grid_size-1])
+            6. self.grid_centers (Numpy array containing the raveled grid center coordinates, shape [(grid_size-1)**dimensions, dimensions])
+            7. self.grid_volumes ( Numpy array volumes of every cell, shape [(grid_size-1)**dimensions])
+
+        Args:
+            None
+        Returns: 
+            None
+        """
+
+        # Calculate 1000x1000 grid in parameter space
+        # should be grid_size**d elements
+        self.grid, self.grid_mesh, self.grid_corners = calculate_square_grid_coordinates(self.gridsize, self.bounds)
+
+        # Calculate bin centers
+        # should be (grid_size-1)**d elements
+        self.grid_centers, self.grid_mesh_centers, self.grid_centers = calculate_square_grid_centers(self.grid)
+
+        # Calculate grid volumes - Assume fixed grid
+        # should be (grid_size-1)**d elements
+        self.grid_volumnes = np.product(np.array([self.grid[p][1]-self.grid[p][0] for p in self.grid.keys()])) * np.ones(self.grid_centers.shape)
+        return
+
+    def _train_base_model_kde(self,population_model):
+        pop_model_samples = np.vstack([ population_model.get_samples(class_name, parameters) for class_name in population_model.classes])
+        base_model_kde = self.kde(pop_model_samples,**kde_kwargs)
+        return base_model_kde
+
+        
     def apply_uq(self, unnormalized_prob,inference_data, population_model, parameters):
         """
         Applies ``None'' class uncertainty quantification to the classification results. 
@@ -42,31 +88,10 @@ class NoneClassUQ(additiveUQ):
             probability, unnormalized, with the appended ``None'' class and associated probability.
         
         """
-        # Tunable Hyperparameters: kde method, kde kernel (maybe), kde bandwidth, prior weight
-        grid_size = int(1e3)
-        kde = gaussian_kde
-        kde_kwargs = {}
-
-
-        # Calculate 1000x1000 grid in parameter space
-        # should be grid_size**d elements
-        grid, grid_mesh, grid_corners = calculate_square_grid_coordinates(gridsize, bounds)
-
-        # Calculate bin centers
-        # should be (grid_size-1)**d elements
-        grid_centers, grid_mesh_centers, grid_centers = calculate_square_grid_centers(grid)
-        #grid_centers = {p : (grid[p][1:] + grid[p][:-1])/2 for p in grid.keys()}
-        #grid_mesh_centers = np.array(np.meshgrid(*list(grid_centers.values())))
-        #grid_centers = np.array([ a.ravel() for a in grid_mesh_centers]).transpose()
-
-        # Calculate grid volumes - Assume fixed grid
-        # should be (grid_size-1)**d elements
-        grid_volumnes = np.product(np.array([grid[p][1]-grid[p][0] for p in grid.keys()])) * np.ones(grid_centers.shape)
-
         # Train kde with tophat kernel and bandwidth of .4 on galactic model data
-        pop_model_samples = np.vstack([ population_model.get_samples(class_name, parameters) for class_name in population_model.classes])
-        pop_model_kde = kde(pop_model_samples,**kde_kwargs)
-        pop_model_eval_centers = pop_model_kde(grid_centers)
+        if self.base_model_kde is None:
+            self._train_base_model_kde(population_model) 
+        pop_model_eval_centers = self.base_model_kde(self.grid_centers)
         max_pop_model_eval_centers = np.amax(pop_model_eval_centers)
 
         # Assign p(phi|NONE) to bin centers
