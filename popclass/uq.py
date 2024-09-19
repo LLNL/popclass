ec7088ec1414e88f3bd91d0a4ed3cf74404ae94e
Zofia Kaczmarek <zofiak@zah-153.zah.uni-heidelberg.de>
2024-09-19 17:44:21 +0200

    Updated docstrings

--- popclass/uq.py
+++ popclass/uq.py
@@ -176,11 +176,11 @@ class NoneClassUQ(additiveUQ):
     
     def evaluate(self, posterior):
         """
-        Evaluates 
+        Evaluates the pre-constructed None class probability for a popclass.Posterior object, returning p(sample parameter values | None class, model) for each sample in the provided posterior distribution.
         
         Args:
             posterior (popclass.Posterior):
-                
+                    popclass Posterior object containing samples.
             
         Returns:
             eval_ (numpy.ndarray):
